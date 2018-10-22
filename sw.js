c64d7e810190e45b68dd1af7d27280209e47a116
Matthew Smith <mateoelcule@gmail.com>
2018-10-21 18:12:05 -0700

    Added the 'install', 'activate' and 'fetch' event listeners to 'sw.js'.

--- sw.js
+++ sw.js
@@ -1,3 +1,46 @@
+const appName = 'restaurant-reviews-app';
+const staticCacheName = appName + 'v1';
+
+self.addEventListener('install', function(event) {
+
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        'restaurant.html',
+        'css/styles.css',
+        'js/dbhelper.js',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'data/restaurants.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith(appName) &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
-     console.log(event.request);
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetch(event.request);
+    })
+    .catch(function(error) {
+     console.log(error);
+    })
+  );
 });
