d504f72feeeb4bbeac37325dd7fcd59d8bdde61b
Matthew Smith <mateoelcule@gmail.com>
2018-10-22 22:36:37 -0700

    Added functionality to save resource requests from the network to the cache on 'fetch'.

--- sw.js
+++ sw.js
@@ -33,9 +33,13 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      return response || fetch(event.request);
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).then(function(response) {
+        return caches.open(staticCacheName).then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+          });
+      });
     })
     .catch(function(error) {
      console.log(error);
